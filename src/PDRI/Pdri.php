60429b0d51f20232a490892fdb619af3db74dfa3
ariaarub <95572966+ariaarub@users.noreply.github.com>
2022-11-18 13:29:49 +0700

    realize: calculate total import

--- src/PDRI/Pdri.php
+++ src/PDRI/Pdri.php
@@ -7,27 +7,51 @@ class Pdri{
     private float $payablePph;
     private float $payablePpnBm;
     private float $totalImportFee;
+
+    private Product $product;
     
 
-    public function __construct(){
-
+    public function __construct(Product $product){
+        $this->product = $product;
     }
     
     public function calculatePayablePpn(float $importPayable): float{
-        $Ppn = new PpnTariff();
-        return 0.1;
+        $Ppn = new PpnTariff($this->product);
+
+        return $Ppn->calculatePpn($importPayable);
     }
 
     public function calculatePayablePph(): float{
+        #TODO: Make selector depending on NPWP/API availability
         return 0.2;
     }
 
     public function calculatePayablePpnBm(): float{
+        #TODO: Optional function. Has complicated procedure
+
+        /* PPN = Tarif PPN x (Harga Barang – PPnBM)
+
+            PPN = 10% x (Rp2.000.000.000 – (Rp2.000.000.000 x 40%))
+
+            PPN = 10% x (Rp2.000.000.000 –800.000.000)
+
+            PPN = 10% x Rp1.200.000.000 =Rp120.000.0000
+
+            Jadi, total harga mobil yang harus dibayarkan Andi adalah:
+
+            =Harga Mobil + PPN + PPnBM 
+
+            =Rp2.000.000.000+Rp120.000.000+Rp800.000.000
+
+            =Rp2.920.000.000 
+        */
         return 0.3;
     }
 
-    public function calculateTotalImportFee(): float{
-        return 0.4;
+    public function calculateTotalImportFee(float $payablePpn, float $payablePph, float $payableImport): float{
+        
+        $this->totalImportFee = $payablePpn + $payablePph + $payableImport;
+        return $this->totalImportFee;
     }
 
     public function getPayablePpn(): float{
