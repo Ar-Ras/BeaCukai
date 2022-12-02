515aea8256604c5ea1d7f8e4b4d6f7d382ba14af
ariaarub <95572966+ariaarub@users.noreply.github.com>
2022-12-02 12:59:16 +0700

    Revert "Revert "psr-4 standard""
    This reverts commit 3ca62117edce0222c2288e5fcbafa6d168724d05.
    
    	new file:   src/Cfg/Configurator.php
    	new file:   src/Cif/Cif.php
    	new file:   src/Pdri/Pdri.php
    	new file:   src/Pph/Owners/ApiOwner.php
    	new file:   src/Pph/Owners/NonNpwpOwner.php
    	new file:   src/Pph/Owners/NpwpOwner.php
    	new file:   src/Ppn/PpnBmTariff.php
    	new file:   src/Ppn/PpnTariff.php

---
+++ src/Cif/Cif.php
@@ -0,0 +1,32 @@
+<?php
+namespace ArRas\BeaCukai\Cif;
+
+#TODO
+
+class Cif{
+     private float $insuranceCost;
+     private float $shippingCost;
+     private float $goodsCost;
+     private float $CIF;
+     private float $customsFee;
+
+
+      public function __construct(){
+         #TODO
+      }
+
+     public function calculateCIF(float $insuranceCost, float $shippingCost, $goodsCost): float{
+         $this->CIF = $insuranceCost + $shippingCost + $goodsCost;
+
+         return $this->CIF;
+     }
+
+     public function calculateCustomsFee(float $importFee): float {
+         $this->customsFee = $importFee * $this->CIF;  
+         return $this->customsFee;
+     }
+
+     public function calculateTotalImportFee(): float {
+        return $this->customsFee + $this->goodsCost;
+     }
+}
